48eef053dfb1c29ac664da69fee61c268856846b
citizen-nsl <155259170+citizen-nsl@users.noreply.github.com>
2024-04-18 00:27:46 +0700

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
     production: {
         bot: {
-            token: 'MTIyOTcyNjI4MTQwMzIwMzYwNQ.GDPzaC.uo_dsIqbgEXZOfmLoN1TZW9LsUN973V_2vN5Tk', 
+            token: ''
         }
     }
 }
